f50e26e04c2975b7ff173770489e392ab99ef333
rismamutia <rismamutiadewi89@gmail.com>
2025-05-08 16:34:39 +0700

    berfungsi untuk menampilkan informasi mahasiwa berdasarkan NIM

--- linkedList.cpp
+++ linkedList.cpp
@@ -93,5 +93,110 @@ class List
         return true;
     }
 
-    
-}
+    void traverse()
+    {
+        if (listEmpty())
+    {
+        cout << "\nList Kosong\n";
+    }
+    else
+    {
+        cout << "\nData didalam list adalah:\n";
+        Node *currentNode = START;
+        while (currentNode != NULL)
+        {
+            cout << currentNode->noMhs << endl;
+            currentNode = currentNode->next;
+        }
+        cout << endl;
+    }
+    }
+};
+
+    int main()
+        {
+            List mhs;
+            int nim;
+            char ch;
+            while (1)
+
+            do
+            {
+                cout << "Menu" << endl;
+                cout << endl;
+                cout << "1. Menambah data kedalam list" << endl;
+                cout << "2. Menghapus data dari dalam list" << endl;
+                cout << "3. Menampilkan semua data didalam list" << endl;
+                cout << "4. Mencari data dalam list" << endl;
+                cout << "5. Keluar" << endl;
+                cout << endl
+                    << "Masukkan pilihan (1-5): ";
+                cin >> ch;
+                switch (ch)
+                {
+                    case '1':
+                    {
+                        mhs.addNode();
+                        break;
+                    }
+
+                    case '2':
+                    {
+                        if (mhs.listEmpty())
+                        {
+                            cout << endl
+                                 << "List Kosong" << endl;
+                            break;
+                        }
+                        cout << endl
+                            << "\nMasukkan no mahasiswa yang akan di hapus : ";
+                        cin >> nim;
+                        if (mhs.delNode(nim) == false)
+                        cout << endl
+                            << "Data tidak ditemukan" << endl;
+                        else
+                        cout << endl
+                            << "Data dengan nomor mahasiswa " << nim << "berhasil dihapus " << endl;
+                    }
+                    break;
+                    case '3':
+                    {
+                        mhs.traverse();
+                    }
+                    break;
+
+                    case '4':
+                    {
+                        if (mhs.listEmpty() == true)
+                        {
+                            cout << "\nList Kosong\n";
+                            break;
+                        }
+                        Node *previous, *current;
+                        cout << endl
+                            << "Masukkan no mahasiswa yang dicari : ";
+                        cin >> nim;
+                        if (mhs.search(nim, &previous, &current) == false)
+                        cout << endl  
+                            << "Data tidak ditemukan" << endl;
+                        else
+                        {
+                            cout << endl
+                                << "Data ditemukan" << endl;
+                            cout << "\nNo Mahasiswa: " << current->noMhs << endl;
+                            cout << "\n";
+                        }                    
+                     }
+                break;
+                case '5':
+                {
+                }
+                break;
+                default:
+                {
+                    cout << "Pilihan salah !." << endl;
+                }
+                break;
+            }
+        } while (ch != '5');
+    }
