ab580e857e758f562e876a441f77ddd68bee4918
rismamutia <rismamutiadewi89@gmail.com>
2025-05-08 15:41:26 +0700

    menghapus node dari linked list nim

--- linkedList.cpp
+++ linkedList.cpp
@@ -73,5 +73,25 @@ class List
             *previous = *current;
             *current = (*current)->next;
         }
+
+        return (*current != NULL);
     }
+
+    bool delNode(int nim)
+    {
+        Node *current, *previous;
+        if (!search(nim, &previous, &current))
+        return false;
+
+        if (current == START)
+        START = START->next;
+
+        else
+        previous->next = current->next;
+
+        delete current;
+        return true;
+    }
+
+    
 }
